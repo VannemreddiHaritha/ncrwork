d78a2bc449aaa17d9e7374a17a7d3da8c3be24a8
haritha <haritha@LAPTOP-1KKBO5U4>
2019-02-03 12:08:12 +0530

    added a palindrome

---
+++ palindromeQ5/palindromeQ5/palindromeQ5.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int n,rev=0,r,num;
+	scanf_s("%d", &n);
+	num = n;
+	while (n != 0)
+	{
+		r = n % 10;
+		rev = rev * 10 + r;
+		n = n / 10;
+	}
+	if (num == rev)
+		printf("palindrome");
+	else
+		printf("not  palindrome");
+
+	getch();
+	return 0;
+}

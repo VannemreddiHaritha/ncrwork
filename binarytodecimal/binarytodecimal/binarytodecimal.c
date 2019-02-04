78dee17b190d4c1093d65c362b3b456a7dc8819f
haritha <haritha@LAPTOP-1KKBO5U4>
2019-02-04 20:43:01 +0530

    added a binary to decimal program final

--- binarytodecimal/binarytodecimal/binarytodecimal.c
+++ binarytodecimal/binarytodecimal/binarytodecimal.c
@@ -3,13 +3,14 @@
 #include<math.h>
 int main()
 {
-	int n, B=0, i = 0, r;
-	scanf_s("%d", &n);
+	long int n;
+	int B=0, i = 0, r;
+	scanf_s("%ld", &n);
 	while (n != 0)
 	{
 		r = n % 10;
-		n = n / 10;
 		B = r * pow(2 , i) + B;
+		n = n / 10;
 		i++;
 	}
 	printf("%d", B);

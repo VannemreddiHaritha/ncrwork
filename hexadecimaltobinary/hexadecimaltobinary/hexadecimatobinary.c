0a0ca85e150e242a83f4833884599de2189ad652
haritha <haritha@LAPTOP-1KKBO5U4>
2019-02-06 16:07:36 +0530

    hexa

--- hexadecimaltobinary/hexadecimaltobinary/hexadecimatobinary.c
+++ hexadecimaltobinary/hexadecimaltobinary/hexadecimatobinary.c
@@ -1,40 +1,37 @@
 #include<stdio.h>
-#include<stdlib.h>
+#include<conio.h>
+#include<string.h>
 int main()
 {
-	char a[1000], b[1000];
+	char a[100] = "";
+	char s[100];
 	int i = 0;
-	scanf_s("%d", a);
-	while (a[i] != '\0')
+	printf("enter hexa");
+	gets(s);
+	while (s[i] != '\0')
 	{
-		switch (a[i])
+		switch (s[i])
 		{
-		case '0': "0000"; break;
-		case '1': printf("0001"); break;
-		case '2': printf("0010"); break;
-		case '3': printf("0011"); break;
-		case '4': printf("0100"); break;
-		case '5': printf("0101"); break;
-		case '6': printf("0110"); break;
-		case '7': printf("0111"); break;
-		case '8': printf("1000"); break;
-		case '9': printf("1001"); break;
-		case 'A': printf("1010"); break;
-		case 'B': printf("1011"); break;
-		case 'C': printf("1100"); break;
-		case 'D': printf("1101"); break;
-		case 'E': printf("1110"); break;
-		case 'F': printf("1111"); break;
-		case 'a': printf("1010"); break;
-		case 'b': printf("1011"); break;
-		case 'c': printf("1100"); break;
-		case 'd': printf("1101"); break;
-		case 'e': printf("1110"); break;
-		case 'f': printf("1111"); break;
-		default:  printf("\nInvalid hexadecimal digit %c ", a[i]);
-		}i++;
-	
+		case('0'): {strcat(a, "0000");break;}
+		case('1'): {strcat(a, "0001");break;}
+		case('2'): {strcat(a, "0010");break;}
+		case('3'): {strcat(a, "0011");break;}
+		case('4'): {strcat(a, "0100");break;}
+		case('5'): {strcat(a, "0101");break; }
+		case('6'): {strcat(a, "0110");break; }
+		case('7'): {strcat(a, "0111");break;}
+		case('8'): { strcat(a, "1000");break;}
+		case('9'): { strcat(a, "1001");break;}
+		case('a'): { strcat(a, "1010");break;}
+		case('b'): { strcat(a, "1011");break; }
+		case('c'): { strcat(a, "1100");break;}
+		case('d'): { strcat(a, "1101");break;}
+		case('e'): { strcat(a, "1110");break;}
+		case('f'): {strcat(a, "1111");break; }
+		}
+		i++;
 	}
+	puts(a);
 	getch();
 	return 0;
 }

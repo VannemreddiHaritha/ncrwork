8631bd2d126e6a221bf6e89afbb0b6744d5c3f0a
haritha <haritha@LAPTOP-1KKBO5U4>
2019-02-10 17:02:32 +0530

    added a invertponwards

---
+++ ass4_q8/ass4_q8/ass4_q8.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	char *a[4];
+	int n, i, j;
+	int x, y, k;
+	for (i = 0;i < 4;i++)
+	{
+		scanf("%d", n);
+		a[i] = (char *)malloc(n * sizeof(char));
+		scanf("%s", a[i]);
+		if (a[i] == NULL)
+		{
+			for (j = i;j >= 0;j--)
+			{
+				free(a[i]);
+			}
+			return -1;
+		}
+		printf("%s", a[i]);
+	}
+
+
+
+
+	getch();
+	return 0;
+}

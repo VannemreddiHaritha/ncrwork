adad8dad35f5fa080543307e681e3d5ab56b2506
haritha <haritha@LAPTOP-1KKBO5U4>
2019-02-15 14:43:53 +0530

    addd a employee program

--- c++_ass1_q8/c++_ass1_q8/c++_ass1_q8.cpp
+++ c++_ass1_q8/c++_ass1_q8/c++_ass1_q8.cpp
@@ -1,17 +1,51 @@
 #include<iostream>
+#include<string.h>
 using namespace std;
+
 class employee
 {
-	char name[2][2];
+	char name[30];
 	int salary;
+public:
 	employee()
 	{
-		name[][] = NULL;
+		strcpy(name,"\0");
 		 salary = 0;
+		 cout << "default constructor " << endl;
 	}
-	employee(char s[], int i)
+	employee(int s)
 	{
-	       
-		int salary=
+		salary = s;
+		cout << "parameterized constructor" << endl;
+		
+	}
+	employee(char *n, int s)
+	{
+		salary = s;
+		strcpy_s(name, 5,n);
+		cout << "parameterized constructor with two arguments" << endl;
+	}
+	employee(employee &e)
+	{
+		salary = e.salary;
+		strcpy(name, e.name);
+		cout << "copy contrustor";
+	}
+	~employee()
+	{
+		cout << "destryctor" << endl;
 	}
 };
+
+int main()
+{
+	
+	employee e1, e2(1000),e3("hari",1000), e4(e3);
+	int i, j;
+	i = sizeof(e3);
+	j = sizeof(e4);
+	cout <<"value of i" <<i << endl <<"value of j"<< j;
+	system("pause");
+	return 0;
+
+}

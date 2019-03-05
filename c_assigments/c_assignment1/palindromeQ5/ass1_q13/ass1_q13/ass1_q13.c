69c107f05d0c1fbdc1c21d842486e6f0f4a84dba
haritha <haritha@LAPTOP-1KKBO5U4>
2019-03-05 09:09:39 +0530

    changes in folder

---
+++ c_assigments/c_assignment1/palindromeQ5/ass1_q13/ass1_q13/ass1_q13.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+int invertponwards(int, int, int);
+int main()
+{
+	int X, P, N, RESULT;
+	scanf_s("%d %d %d", &X, &P, &N);
+	printf("Coverted binary value of %d is\t", X);
+	RESULT = invertponwards(X, P, N);
+	getch();
+	return 0;
+}
+
+	int invertponwards(int X, int P, int N)
+	{
+		int I, Y, LIMIT, LEN, BIN[15];
+		LEN = 0;
+		while (X > 0)
+		{
+			BIN[LEN] = X % 2;
+			X = X / 2;
+			LEN++;
+		};
+
+		for (I = LEN - 1; I > -1; I--)
+		{
+			printf("%d ", BIN[I]);
+		}
+		printf("\n");
+		LIMIT = P + N;
+		if (LIMIT < LEN)
+		{
+			for (I = P + N - 1;I > P - 1; I--)
+			{
+				if (BIN[I] == 0)
+					BIN[I] = 1;
+				else
+					BIN[I] = 0;
+			}
+		}
+		else
+		{
+			printf("YOU HAVE ENTERED LARGE VALUE FOR P AND N");
+		}
+
+		printf("Inverted integer in binary form is \t");
+		for (I = LEN - 1; I > -1; I--)
+		{
+			printf("%d ", BIN[I]);
+		}
+
+}
---
+++ c_assigments/c_assignment1/palindromeQ5/helloworld/ass1_q3_b/ass1_q3_b/ass1_q3_b.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int n=0XCAFE;
+	int r;
+	while (n != 0)
+	{
+		r = n % (pow(10, 8));
+		printf("%x", r);
+		n = n / (pow(10, 8));
+	}
+	getch();
+	return 0;
+}

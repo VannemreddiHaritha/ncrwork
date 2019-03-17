44a254fdb3b6be205cfd80da8572a431e5924c97
haritha <haritha@LAPTOP-1KKBO5U4>
2019-03-17 13:17:41 +0530

    changes

--- c++assignments/c++assignment2/c++_assg2_q3/c++_assg2_q3/c++_assg2_q3.cpp
+++ c++assignments/c++assignment2/c++_assg2_q3/c++_assg2_q3/c++_assg2_q3.cpp
@@ -68,7 +68,7 @@ public:
 	}
 	void print()
 	{
-		cout << "arrow";
+		cout << "The fuction is excuted in arraw overloading";
 	}
 
 	friend istream& operator >> (istream &cin, complex &c);
@@ -82,32 +82,34 @@ istream& operator >> (istream &cin, complex &c)
 }
 ostream& operator<<(ostream &cout, complex c)
 {
-	cout << "real" << c.real << endl;
-	cout << "img" << c.img << endl;
+	cout << "real = " << c.real << endl;
+	cout << "img= " << c.img << endl;
 	return cout;
 }
 int main()
 {
 	complex c1, c2, c3, c4, c5, c6, c7, c8, c9(5, 6), c10(7, 8);
 	int a = 10;
-	cout << "enter the real and img valuse";
+	cout << "enter the real and img values";
 	cin >> c1;
+	cout<<"the first obj values are ";
 	cout << c1 << endl;
-	cout << "enter the real and img valuse";
+	cout << "enter the real and img values";
 	cin >> c2;
+	cout << "the second obj values are ";
 	cout << c2 << endl;
-	c1 = c1 + c2;
-	cout << c1 << endl;
+	c3 = c1 + c2;
+	cout << "sum = "<<c1 << endl;
 	c4 = c1 - c2;
-	cout << "sub" << c4 << endl;
+	cout << "sub = " << c4 << endl;
 	c5 = ++c1;
-	cout << "inc" << c5 << endl;
+	cout << "inc = " << c5 << endl;
 	c6 = c1++;
-	cout << "dec" << c6 << endl;
+	cout << "dec= " << c6 << endl;
 	c7 = (c1, c2);
-	cout << "comma" << c7 << endl;
+	cout << "comma= " << c7 << endl;
 	c1->print();
-	cout << "arrow" ;
+	
 
 	system("pause");
 	return 0;
